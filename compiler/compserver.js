46275c54dd3759206d6b8c5c8b5626b273aa87ca
Pratham <rimo.coder@gmail.com>
2024-11-13 11:41:12 +0530

    Test

--- compiler/compserver.js
+++ compiler/compserver.js
@@ -8,9 +8,9 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const corsOptions = {
-  // origin: "https://algohub7.vercel.app", // Replace with your frontend origin
+  origin: "https://algohub7.vercel.app", // Replace with your frontend origin
   // origin: "http://localhost:5173",
-  // origin: "*",
+  // origin: "*", // * doesnt allow the tokens to be included
   credentials: true, // Include cookies if necessary
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   methods: 'GET, POST, PUT, DELETE, OPTIONS', // Allowed HTTP methods
